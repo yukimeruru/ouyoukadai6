95f6059d7e75cd4d4cf05ef991808ac203c087b1
yuki-meruru <yuki725421@gmail.com>
2021-10-04 12:41:25 +0000

    応用課題５

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'homes#top'
+
   get 'home/about' => 'homes#about'
+
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get :followers, on: :member
     get :followeds, on: :member
   end
+
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments,only: [:create, :destroy]
